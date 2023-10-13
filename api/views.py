98bc7723c4d273b31fb71b089abc7fef1e667c32
Ckefa <ckefa65@gmail.com>
2023-10-13 23:30:34 +0300

    adding wsgi app

--- api/views.py
+++ api/views.py
@@ -1,7 +1,20 @@
 from api import base
-from flask import render_template
+from flask import render_template, request
 
 
 @base.route("/", strict_slashes=False)
 def home():
     return render_template("index.html")
+
+
+@base.route("/pay", methods=["GET", "POST"], strict_slashes=False)
+def pay():
+    data = request.json
+    print(data)
+    if request.method == "GET":
+        return "Payment confirmed"
+
+    elif request.method == "POST":
+        return "Payment Success"
+    else:
+        return {"msg": "error occured"}
